107e6afeef88653a0ed884576881075ddc469b1b
mmarszik <mmarszik@gmial.com>
2020-01-27 14:54:33 +0100

    .

--- run/run.sh
+++ run/run.sh
@@ -1,27 +1,28 @@
-if [ -f MKnapsack ]; then
+if [ ! -f MKnapsack ]; then
+echo "compile"
 g++ -std=c++14 -O3 -march=native -mtune=native -DUSE_RND_MT19937_64 -DUSE_RND_BUFF_1 -DUSE_RND_F_BUFF_1 -DUSE_RND_PROB_0 -I../vendor/ ../vendor/MGenCPP/*.cpp ../src/*.cpp ../vendor/MRndCPP/*.cpp ../vendor/MiscCPP/m_args.cpp -o MKnapsack
 fi
 
 row=$1
-while [ $row -le 5000 ]
+while [ $row -le 2000 ]
 do
 args=$(cat args | head -n$row | tail -n1)
 echo "$row - $args"
 
-echo "$row ---------------------------------------------" >> results.txt
-echo           >> results.txt   
-echo $args     >> results.txt
-echo           >> results.txt
+echo "$row ---------------------------------------------" >> results_$1.txt
+echo           >> results_$1.txt   
+echo $args     >> results_$1.txt
+echo           >> results_$1.txt
 
-r=1395376521
+r=876349874
 {
 time (
-for i in {1..500}
+for i in {1..2000}
 do
     r=$(( (r*103867+824723)%2000000000 ))
     cat data.txt | ./MKnapsack --rndSeed=$r $args  | tail -n5 | head -n1
 done
-) | awk -F';' '{ sum+=sprintf("%f",$3); } END {print sum/500}' >> results_$1.txt; } 2>> results.txt
+) | awk -F';' '{ sum+=sprintf("%f",$3); } END {print sum/2000}' >> results_$1.txt; } 2>> results_$1.txt
 
 row=$((row+4))
 done
